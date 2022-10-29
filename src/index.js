c89cd9a44666b1e2b74ad7a848d584b0cb736af4
phuql <quangphu112@gmail.com>
2022-10-29 15:55:56 +0700

    update code template engine

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import { engine } from 'express-handlebars';
+import express from 'express';
+import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express()
+const port = 3000
+
+// Template Engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'resources\\views'))
+
+
+// HTTP Logger
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
